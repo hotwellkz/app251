b4ab11f7719434631ec461ca66864c7086a5a2bf
hotwellkz <hotwell.kz@gmail.com>
2025-01-29 19:25:55 +0600

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 // Определяем базовый URL API в зависимости от окружения
 export const API_BASE_URL = process.env.NODE_ENV === 'production' 
-    ? 'https://2wix.ru'  // Замените на URL вашего развернутого сервера
+    ? 'http://localhost:3000'  // Замените на URL вашего развернутого сервера
     : 'http://localhost:3000';
 
 // Конфигурация для Socket.IO
